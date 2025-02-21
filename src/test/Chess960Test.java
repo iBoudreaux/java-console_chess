6ff8ce9956b33359fa89acac689bb1cb452e4e23
Isabella Boudreaux <iboudreaux@student.neumont.edu>
2025-02-20 17:50:26 -0700

    Seeing if this fixes anything

---
+++ src/test/Chess960Test.java
@@ -0,0 +1,9 @@
+// package test;
+
+// import org.junit.Test;
+// import static org.junit.Assert.*;
+
+// public class Chess960Test {
+// 	// 5 tests to make sure the project runs as intended
+
+// }
